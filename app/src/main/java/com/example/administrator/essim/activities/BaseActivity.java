8b5e5d5d172f574d4f38298101e2197b63476834
CeuiLiSA <32817377+CeuiLiSA@users.noreply.github.com>
2018-10-05 12:52:49 +0800

    重新开启腾讯统计SDK

--- app/src/main/java/com/example/administrator/essim/activities/BaseActivity.java
+++ app/src/main/java/com/example/administrator/essim/activities/BaseActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
-import com.example.administrator.essim.R;
 import com.example.administrator.essim.interf.TapToRefresh;
 
 public class BaseActivity extends AppCompatActivity {
